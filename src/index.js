f9141df4daeb14f469f7b51e8a0b5a39a4ab1e85
user <sivtsovatanya1985@gmail.com>
2022-07-09 17:57:13 +0300

    created a new project test

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-let img = document.querySelector('.img');
-let src1 = './images/img1.png';
-let src2 = './images/img2.png';
-let srcArr = [src1, src2];
+const imageEl = document.querySelector('.img');
+const source1 = './images/img1.png';
+const source2 = './images/img2.png';
+const imagesSources = [source1, source2];
 
-
-
-let count = 0;
+let currentImageIndex = 0;
 
 
 function displayImage() {
+	const imageIndex = currentImageIndex % imagesSources.length;
 
-	setInterval(e => {
-		let randomNumber = Math.floor(Math.random() * 3);
-		count = (count + randomNumber) % srcArr.length;
-		let newColour = srcArr[count];
-		document.getElementById('img').src = `#${newColour}`;
-		document.innerHTML = img;
+	imageEl.src = imagesSources[imageIndex];
 
-	}, 1000)
+	currentImageIndex++;
+
+	const delay = currentImageIndex % imagesSources.length === 0 ? 2000 : 300;
 
 	setTimeout(() => {
 
 		displayImage();
 
-	}, 5000);
+	}, delay);
 }
-
 
 displayImage();
